7ab8291b7740c4375303b6d7e20790240c6a59b8
bispoalef <alef.b.dos.santos@hotmail.com>
2023-01-23 20:07:41 -0300

    primeira entidade do projeto

---
+++ src/main/java/br/com/ProjetoIntroducaoSpring/resources/UsuarioResource.java
@@ -0,0 +1,23 @@
+package br.com.ProjetoIntroducaoSpring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ProjetoIntroducaoSpring.entites.Usuario;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioResource {
+
+	
+	
+	
+	@GetMapping
+	public ResponseEntity<Usuario> findAll() {
+
+		Usuario user = new Usuario(1L, "alef", "email", "7999", "123");
+		return ResponseEntity.ok().body(user);
+	}
+}
